04426a79d26f4ee096a0f6a665d74750e80e9bf2
Sergey Shevchenko <skynowa@fabrica.net.ua>
2019-03-13 09:49:38 +0200

    modulePath: platform imp

--- StdTest.h
+++ StdTest.h
@@ -110,25 +110,60 @@ currentDateTime()
 inline std::string
 modulePath()
 {
-    std::string srv;
+    std::string sRv;
 
-#if defined(KERN_PROC_PATHNAME)
-    const u_int mibSize            {4};
-    int         mib[mibSize]       {CTL_KERN, KERN_PROC, KERN_PROC_PATHNAME, - 1};
-    char        buff[PATH_MAX + 1] {};
-    std::size_t buffSize           {sizeof(buff) - 1};
+#if   defined(_MSC_VER)
+    sRv.resize(MAX_PATH);
 
-    int iRv = ::sysctl(mib, mibSize, buff, &buffSize, nuulptr, 0U);
+    DWORD stored = ::GetModuleFileName(nullptr, &sRv.at(0), static_cast<DWORD>( sRv.size() ));
+    if (stored == 0UL) {
+        return "<unknown>";
+    }
+
+    sRv.resize(stored);
+#elif defined(__linux__)
+	const std::string procFile = "/proc/" + std::to_string(::getpid()) + "/exe";
+
+	int         readed {- 1};
+	std::string filePath(PATH_MAX, '\0');
+
+	for ( ; ; ) {
+		readed = ::readlink(procFile.c_str(), &filePath.at(0), filePath.size());
+		if (readed == - 1) {
+			return "<unknown>";
+		}
+
+		if (filePath.size() > static_cast<size_t>(readed)) {
+			break;
+		}
+
+		filePath.resize(filePath.size() * 2);
+	}
+
+	filePath.resize( static_cast<size_t>(readed) );
+
+	sRv.assign(
+		std::find(filePath.rbegin(), filePath.rend(), '/').base(),
+		filePath.end());
+#elif defined(__FreeBSD__)
+    constexpr u_int mibSize            {4};
+    int             mib[mibSize]       {CTL_KERN, KERN_PROC, KERN_PROC_PATHNAME, - 1};
+    char            buff[PATH_MAX + 1] {};
+    std::size_t     buffSize           {sizeof(buff) - 1};
+
+    int iRv = ::sysctl(mib, mibSize, buff, &buffSize, nulptr, 0U);
     if (iRv == - 1) {
         return "<unknown>";
     }
 
-    srv.assign(buff);
+    sRv.assign(buff);
+#elif defined(__APPLE__)
+    // TODO: module path
 #else
-    srv = "<unknown>";
+    sRv = "<unknown>";
 #endif
 
-    return srv;
+    return sRv;
 }
 //-------------------------------------------------------------------------------------------------
 
